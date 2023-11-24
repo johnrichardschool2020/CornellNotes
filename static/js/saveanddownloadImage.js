b9485ea0622dcf2788e10c3372180ede4bb146be
John Richard Orian <johnrichardschool2020@gmail.com>
2023-11-24 19:47:42 +0800

    Save XML and Download as JPEG

--- static/js/saveanddownloadImage.js
+++ static/js/saveanddownloadImage.js
@@ -1,4 +1,4 @@
-function saveData() {
+function downloadImage() {
         //SAVE XML
         var title = document.querySelector('.notetitle').innerText;
         var keyword = document.querySelector('.notekeyword').innerText;
@@ -21,4 +21,17 @@ function saveData() {
         .then(response => response.text())
         .then(data => console.log(data))
         .catch(error => console.error('Error:', error));
+
+
+    // DOWNLOAD AS JPEG
+    html2canvas(document.querySelector('.cornellnotesdoc')).then(canvas => {
+        // Convert the canvas to a data URL
+        var imageData = canvas.toDataURL('image/jpeg', 1.0);
+
+        // Create a download link
+        var link = document.createElement('a');
+        link.href = imageData;
+        link.download = 'cornell_note.jpg';
+        link.click();
+    });
 }
